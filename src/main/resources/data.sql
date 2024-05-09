0f63f2b8dd226847fd5a85b4e0593cb4630d15f3
Duvan Arley Botero Rivillas <duvabori@suramericana.com.co>
2024-05-09 10:32:33 -0500

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO location (id, name, address) VALUES (1, 'Aeropuerto Olaya Herrera','Medellín, Colombia');
+INSERT INTO location (id, name, address) VALUES (2, 'Aeropuerto Los Garzones','Montería, Colombia');
+INSERT INTO location (id, name, address) VALUES (3, 'LAX Airport','Los Angeles airport');
+INSERT INTO location (id, name, address) VALUES (4, 'Aeropuerto Barajas','Madrir, España');
+INSERT INTO location (id, name, address) VALUES (5, 'Aeropuerto El Dorado','Bogotá, Colombia');
